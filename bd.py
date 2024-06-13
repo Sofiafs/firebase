b596800df94ebb04cc761c75537049195d308a68
Sofia <161875100+Sofiafs@users.noreply.github.com>
2024-06-13 20:32:06 -0300

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,4 +1,4 @@
-import requests
+'''import requests
 import json
 
 link = "https://noble-might-399221-default-rtdb.firebaseio.com/"
@@ -7,3 +7,9 @@ dados = {'email': 'adna@gmail.com', 'senha': 5678}
 requisicao = requests.post(f'{link}/login/.json', data=json.dumps(dados))
 print(requisicao)
 print(requisicao.text)
+
+
+cred = credentials.Certificate('C:/Users/sofia/Downloads/noble-might-399221-firebase-adminsdk-blpau-93e21293d0.json') 
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://noble-might-399221-default-rtdb.firebaseio.com/'  
+})'''
